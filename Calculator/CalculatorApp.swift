eaf1ce7e16fb180d4d87791ad7fd30baf6d89c2f
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-04 20:52:46 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ahmet Ozen on 4.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
