32e2b5bc3ec1dc751ed3255e09df73a59f6cc018
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-05 16:36:23 +0300

    working on iPad support

--- Calculator/CalculatorApp.swift
+++ Calculator/CalculatorApp.swift
@@ -11,7 +11,12 @@ import SwiftUI
 struct CalculatorApp: App {
     var body: some Scene {
         WindowGroup {
-            CalculatorView()
+            if UIDevice.isIPad {
+                Text("iPad")
+            } else {
+                CalculatorView()
+            }
+            
         }
     }
 }
---
+++ Calculator/Helper Functions/iPad Support.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+
+extension UIDevice {
+    static var isIPad: Bool {
+        UIDevice.current.userInterfaceIdiom == .pad
+    }
+    
+    static var isIPhone: Bool {
+        UIDevice.current.userInterfaceIdiom == .phone
+    }
+}
